1f2a4f8bbcf9f17c7524f255c2095ebe9357a692
Macey Ragbir <maceyragbir@gmail.com>
2026-03-22 13:13:19 -0400

    new 8 test cases pass

--- app/models/user_pokemon.py
+++ app/models/user_pokemon.py
@@ -5,4 +5,4 @@ class UserPokemon(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     user_id: int = Field(foreign_key="user.id")
     pokemon_id: int = Field(foreign_key="pokemon.id")
-    nickname: str
+    nickname: str = Field(max_length=225)
